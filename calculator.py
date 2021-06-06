3439b6e7bab4834db893f038554cf36ee35050b6
eliosipov21 <eli.osipov2@gmail.com>
2021-06-06 09:51:34 -0700

    fasdkjf

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
 """
-calc up using circle ci.
+calc up using circle ci..
 """
-def add(a,b):
+
+
+def add(a, b):
     return a + b
 
-def subtract(a,b):
-    return a - b 
+
+def subtract(a, b):
+    return a - b
